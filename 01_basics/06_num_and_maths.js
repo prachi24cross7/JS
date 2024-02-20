3df2356cacb95c9a2898a0b0c088eda795c14f40
PRACHI SINGH <43330553+prachi24cross7@users.noreply.github.com>
2024-02-20 13:25:01 +0000

    number

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,15 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(500);
+console.log(balance);
+
+console.log(typeof balance.toString());
+
+console.log(balance.toFixed(2));
+
+const otherNumber = 236387.94494
+console.log(otherNumber.toPrecision(6));
+
+const hundreds = 1000000000
+console.log(hundreds.toLocaleString('en-IN'));
