29bdc35df037590d21d1030b3be61b3a5a61daee
PRACHI SINGH <43330553+prachi24cross7@users.noreply.github.com>
2024-02-21 06:08:21 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,12 @@
+function one() {
+    const username = "Prachi"
+
+    function two() {
+        const age = 29
+        console.log(username);
+        
+    }
+    //console.log(age);
+    two()
+}
+one()
